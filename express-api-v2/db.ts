d770a6d16d1298d928e3c5c18b68b125071647b2
Erick Weil <erick.weil@ifro.edu.br>
2026-08-14 20:08:38 -0400

    Atualizado .env

--- express-api-v2/db.ts
+++ express-api-v2/db.ts
@@ -2,10 +2,10 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 //import { users } from './drizzle/schema'
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.POSTGRES_URL;
 
 if(!connectionString) {
-    throw new Error("DATABASE_URL environment variable is not set");
+    throw new Error("POSTGRES_URL environment variable is not set");
 }
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
